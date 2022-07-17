dd4a3c05cc90e6c37b9f1136f569aee24ef2d675
Dmitriy Yurov <dyurov@tradingview.com>
2022-07-17 17:40:53 +0300

    renew plugins and deprecate dynamic-takes

--- packages/virtual-takes/index.ts
+++ packages/virtual-takes/index.ts
@@ -1,11 +1,12 @@
-import { PluginInterface, ExecutedOrder, DebutCore, Candle, OrderType } from '@debut/types';
+import { PluginInterface, ExecutedOrder, DebutCore, Candle, OrderType, PluginCtx } from '@debut/types';
 
 export type VirtualTakesOptions = {
-    stopLoss: number; // Stop in percent 12 = 12%
-    takeProfit: number; // Take in percent 20 = 20%
-    trailing?: number; // 1 or 2; 1 - Trailing from start | 2 - after take trailing | 3 - trailing after each take
+    stopLoss?: number; // Stop in percent 12 = 12%
+    takeProfit?: number; // Take in percent 20 = 20%
+    trailing?: TrailingType; // 1 or 2; 1 - Trailing from start | 2 - after take trailing | 3 - trailing after each take
     ignoreTicks?: boolean;
     manual?: boolean; // manual control
+    maxRetryOrders?: number; // how many order can be retried after stop reached
 };
 
 export const enum TrailingType {
@@ -15,22 +16,32 @@ export const enum TrailingType {
     MoveAfterEachTake,
 }
 
+const enum CloseType {
+    STOP,
+    TAKE,
+}
+
 type OrderTakes = {
     takePrice: number;
     stopPrice: number;
     price: number;
+    tryLeft?: number;
+    tryPrice?: number;
+    retryFor?: number;
 };
 
 type TakesLookup = Map<number, OrderTakes>;
 type TrailingLookup = Set<number>;
 
 interface Methods {
-    setForOrder(cid: number, type: OrderType, price: number): void;
+    setTrailingForOrder(cid: number, takePrice: number, stopPrice: number): void;
+    setPricesForOrder(cid: number, takePrice: number, stopPrice: number): void;
+    setForOrder(cid: number, type: OrderType): void;
     getTakes(cid: number): OrderTakes | undefined;
     isManual(): boolean;
 }
 export interface VirtualTakesPluginAPI {
-    dynamicTakes: Methods;
+    takes: Methods;
 }
 
 export interface VirtualTakesPlugin extends PluginInterface {
@@ -41,40 +52,63 @@ export interface VirtualTakesPlugin extends PluginInterface {
 export function virtualTakesPlugin(opts: VirtualTakesOptions): VirtualTakesPlugin {
     const lookup: TakesLookup = new Map();
     const trailing: TrailingLookup = new Set();
-    async function handleTick(debut: DebutCore, tick: Candle) {
-        const price = tick.c;
+    let price = 0;
+    let ctx: PluginCtx;
+
+    async function handleTick() {
         // Нет заявки активной - нет мониторинга
-        if (!debut.ordersCount) {
+        if (!ctx.debut.ordersCount) {
             return;
         }
 
-        for (const order of [...debut.orders]) {
-            if (!('orderId' in order)) {
+        const orders = [...ctx.debut.orders];
+
+        for (let i = 0; i < orders.length; i++) {
+            const order = orders[i];
+
+            if (!order || !('orderId' in order)) {
                 continue;
             }
 
-            if (trailing.has(order.cid)) {
-                trailingTakes(order, price, lookup);
-
-                if (checkClose(order, price, lookup)) {
-                    await debut.closeOrder(order);
-                }
-
-                continue;
-            }
-
+            const hasTrailing = trailing.has(order.cid);
+            const { data, isLink } = getOrderData(order.cid, lookup);
             const closeState = checkClose(order, price, lookup);
 
-            if (opts.trailing === TrailingType.MoveAfterEachTake && closeState === 'take') {
-                createTrailingTakes(order.cid, order.type, price, opts, lookup);
-            } else if (opts.trailing === TrailingType.StartAfterTake && closeState === 'take') {
-                const data = lookup.get(order.cid) || ({} as OrderTakes);
+            if (isLink) {
+                return;
+            }
 
+            // Update trailings and next check close state
+            if (hasTrailing && opts.trailing !== TrailingType.MoveAfterEachTake) {
+                trailingTakes(order, price, lookup);
+            }
+
+            if (opts.trailing === TrailingType.MoveAfterEachTake && closeState === CloseType.TAKE) {
+                createTrailingTakes(order, price, lookup);
+                trailing.add(order.cid);
+            } else if (opts.trailing === TrailingType.StartAfterTake && closeState === CloseType.TAKE) {
                 data.stopPrice = order.price;
                 data.price = price;
                 trailing.add(order.cid);
-            } else if (closeState) {
-                await debut.closeOrder(order);
+            } else if (closeState === CloseType.STOP && data.tryLeft! > 0 && data.tryPrice) {
+                const priceDiff = price - data.tryPrice;
+
+                data.stopPrice = data.stopPrice + priceDiff;
+                data.takePrice = data.takePrice + priceDiff;
+                data.tryPrice = price;
+
+                // Create same type as origin order
+                const newOrder = await ctx.debut.createOrder(order.type);
+
+                lookup.set(newOrder.cid, { ...data, tryLeft: undefined, retryFor: order.cid });
+                data.tryLeft!--;
+            } else if (closeState === CloseType.STOP || closeState === CloseType.TAKE) {
+                if (opts.maxRetryOrders && data.tryLeft! < opts.maxRetryOrders) {
+                    await ctx.debut.closeAll(true);
+                    return;
+                } else {
+                    await ctx.debut.closeOrder(order);
+                }
             }
         }
     }
@@ -83,7 +117,34 @@ export function virtualTakesPlugin(opts: VirtualTakesOptions): VirtualTakesPlugi
         name: 'takes',
 
         api: {
-            setForOrder(cid: number, type: OrderType, price: number): void {
+            setTrailingForOrder(cid: number, takePrice: number, stopPrice: number) {
+                if (!opts.manual) {
+                    throw 'Virtual Takes Plugin should be in a manual mode for call `setForOrder`';
+                }
+
+                if (opts.trailing === TrailingType.Classic) {
+                    trailing.add(cid);
+                }
+
+                lookup.set(cid, { price, stopPrice, takePrice });
+            },
+            setPricesForOrder(cid: number, takePrice: number, stopPrice: number) {
+                if (!opts.manual) {
+                    throw 'Virtual Takes Plugin should be in a manual mode for call `setForOrder`';
+                }
+
+                if (!takePrice || !stopPrice) {
+                    throw `prices in setForOrder() should be a number, current take: ${takePrice}, stop: ${stopPrice}`;
+                }
+
+                // Only for orders seted up using API
+                lookup.set(cid, { price, takePrice, stopPrice, tryLeft: opts.maxRetryOrders, tryPrice: price });
+
+                if (opts.trailing === TrailingType.Classic) {
+                    trailing.add(cid);
+                }
+            },
+            setForOrder(cid: number, type: OrderType): void {
                 if (!opts.manual) {
                     throw 'Virtual Takes Plugin should be in a manual mode for call `setForOrder`';
                 }
@@ -95,11 +156,15 @@ export function virtualTakesPlugin(opts: VirtualTakesOptions): VirtualTakesPlugi
                 }
             },
             getTakes(cid: number): OrderTakes | undefined {
-                return lookup.get(cid);
+                return getOrderData(cid, lookup).data;
             },
             isManual() {
                 return opts.manual || false;
             },
+        },
+
+        onInit() {
+            ctx = this;
         },
 
         async onOpen(order) {
@@ -119,40 +184,58 @@ export function virtualTakesPlugin(opts: VirtualTakesOptions): VirtualTakesPlugi
             lookup.delete(closing.cid);
         },
 
-        async onCandle(tick) {
+        async onCandle() {
             if (opts.ignoreTicks) {
-                await handleTick(this.debut, tick);
+                await handleTick();
             }
         },
 
         async onTick(tick) {
+            price = tick.c;
+
             if (!opts.ignoreTicks) {
-                await handleTick(this.debut, tick);
+                await handleTick();
             }
         },
     };
 }
 
+function getOrderData(cid: number, lookup: TakesLookup): { data: OrderTakes; isLink: boolean } {
+    let data = lookup.get(cid);
+    let isLink = false;
+
+    if (data?.retryFor) {
+        data = lookup.get(data.retryFor);
+        isLink = true;
+    }
+
+    return { data: data || ({} as OrderTakes), isLink };
+}
+
 /**
  * Проверяем достижение тейка на оснвании текущей цены
  */
-function checkClose(order: ExecutedOrder, price: number, lookup: TakesLookup) {
+function checkClose(order: ExecutedOrder, price: number, lookup: TakesLookup): CloseType | void {
     const { type, cid } = order;
-    const { takePrice, stopPrice } = lookup.get(cid) || ({} as OrderTakes);
+    const { takePrice, stopPrice } = getOrderData(cid, lookup).data;
 
     if ((type === OrderType.BUY && price >= takePrice) || (type === OrderType.SELL && price <= takePrice)) {
-        return 'take';
+        return CloseType.TAKE;
     }
 
     if ((type === OrderType.BUY && price <= stopPrice) || (type === OrderType.SELL && price >= stopPrice)) {
-        return 'stop';
+        return CloseType.STOP;
     }
 
-    return void 0;
+    return;
 }
 
 function createTakes(cid: number, type: OrderType, price: number, opts: VirtualTakesOptions, lookup: TakesLookup) {
     const rev = type === OrderType.SELL ? -1 : 1;
+
+    if (!opts.stopLoss || !opts.takeProfit) {
+        throw new Error('Virtual Takes Plugin needs stopLoss and takeProfit in strategy options');
+    }
 
     // XXX Так как тейки и стопы виртуальные, можем их не делать реальными ценами с шагом
     const stopPrice = price - rev * price * (opts.stopLoss / 100);
@@ -161,27 +244,21 @@ function createTakes(cid: number, type: OrderType, price: number, opts: VirtualT
     lookup.set(cid, { stopPrice, takePrice, price });
 }
 
-function createTrailingTakes(
-    cid: number,
-    type: OrderType,
-    price: number,
-    opts: VirtualTakesOptions,
-    lookup: TakesLookup,
-) {
-    const rev = type === OrderType.SELL ? -1 : 1;
-    const stopPrice = price - rev * price * (opts.stopLoss / 100 / 2);
-    const takePrice = price + rev * price * (opts.takeProfit / 100 / 2);
-
-    lookup.set(cid, { stopPrice, takePrice, price });
-}
-
-function trailingTakes(order: ExecutedOrder, price: number, lookup: TakesLookup) {
-    const { cid } = order;
-    const takes = lookup.get(cid);
+function createTrailingTakes(order: ExecutedOrder, price: number, lookup: TakesLookup) {
+    const takes = getOrderData(order.cid, lookup).data;
 
     if (!takes) {
         return;
     }
+
+    const delta = (price - takes.stopPrice) / 2;
+
+    takes.takePrice += delta;
+    takes.stopPrice += delta;
+}
+
+function trailingTakes(order: ExecutedOrder, price: number, lookup: TakesLookup) {
+    const takes = getOrderData(order.cid, lookup).data;
 
     takes.takePrice = order.type === OrderType.BUY ? Infinity : -Infinity;
 
